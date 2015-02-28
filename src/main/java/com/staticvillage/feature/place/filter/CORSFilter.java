45c7e641045504e8741c2f5ab86b534366804eb6
jparrish832 <jparrish832@gmail.com>
2015-02-28 11:15:30 -0500

    resolves #5 Adding a basic CORS filter

---
+++ src/main/java/com/staticvillage/feature/place/filter/CORSFilter.java
@@ -0,0 +1,34 @@
+package com.staticvillage.feature.place.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by joelparrish on 2/28/15.
+ */
+@Component
+public class CORSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");     //update to restrict scope
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
