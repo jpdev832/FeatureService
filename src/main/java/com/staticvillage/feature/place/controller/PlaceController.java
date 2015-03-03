1781fcae35ad81ff9bfa0b2ff2ce362c20c91e08
jparrish832 <jparrish832@gmail.com>
2015-03-02 22:14:55 -0500

    Resolves #10 standardize REST API

--- src/main/java/com/staticvillage/feature/place/controller/PlaceController.java
+++ src/main/java/com/staticvillage/feature/place/controller/PlaceController.java
@@ -8,6 +8,8 @@ import com.staticvillage.feature.place.model.PlaceResponse;
 import com.staticvillage.feature.place.store.DataStore;
 import com.staticvillage.feature.place.store.MongoDBFeatureStore;
 import com.staticvillage.feature.place.store.MongoDBPlaceStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.UnknownHostException;
@@ -18,19 +20,58 @@ import java.util.Arrays;
  */
 @RestController
 public class PlaceController {
+    private static Logger log = LoggerFactory.getLogger(PlaceController.class);
+
     //Todo - initialize from configuration at later point
     private DataStore<Place> placeStore;
     private DataStore<Feature> featureStore;
 
     public PlaceController(){
+        log.info("initializing stores...");
         placeStore = new MongoDBPlaceStore();
         featureStore = new MongoDBFeatureStore();
+        log.info("stores initialized");
+    }
+
+    /**
+     * Add/Update a new place to the system
+     * @param place place information
+     * @return response
+     */
+    @RequestMapping(value = "/place", method = { RequestMethod.PUT, RequestMethod.POST })
+    public PlaceResponse setPlace(@RequestBody Place place){
+        log.info(String.format("Checking if Place already exists [%s,%s,%s,%s,%s]...", place.getName(),
+                place.getCountry(), place.getState(), place.getCity(), place.getNeighborhood()));
+
+        Place[] places = placeStore.retrieve(place.getId(), place.getCountry(), place.getState(), place.getCity(),
+                place.getNeighborhood(), place.getName());
+
+        if(places != null) {
+            log.info(String.format("Updating {%s}", place.getName()));
+
+            if(placeStore.update(place)) {
+                log.info(String.format("Updated!: %s", place.getId()));
+                return new PlaceResponse(new Place[]{place}, "success");
+            }else {
+                log.info(String.format("Failed to update: %s", place.getName()));
+                return new PlaceResponse(null, "failed");
+            }
+        } else {
+            log.info(String.format("Adding {%s}", place.getName()));
+
+            if (placeStore.insert(place)) {
+                log.info(String.format("Added!: %s", place.getId()));
+                return new PlaceResponse(new Place[]{place}, "success");
+            } else {
+                log.info(String.format("Failed to Add: %s", place.getName()));
+                return new PlaceResponse(null, "failed");
+            }
+        }
     }
 
     /**
      * Get place REST request
      *
-     * @param idStr place id
      * @param country country
      * @param state state
      * @param city city
@@ -39,84 +80,73 @@ public class PlaceController {
      * @return response
      */
     @RequestMapping(value = "/place", method = RequestMethod.GET)
-    public PlaceResponse getPlace(@RequestParam(value = "id", defaultValue = "")String idStr,
-                                  @RequestParam(value = "country", defaultValue = "")String country,
+    public PlaceResponse getPlace(@RequestParam(value = "country", defaultValue = "")String country,
                                   @RequestParam(value = "state", defaultValue = "")String state,
                                   @RequestParam(value = "city", defaultValue = "")String city,
                                   @RequestParam(value = "neighborhood", defaultValue = "")String neighborhood,
                                   @RequestParam(value = "name", defaultValue = "")String name){
-        Place[] places = placeStore.retrieve(idStr, country, state, city, neighborhood, name);
+        log.info(String.format("Checking if Place already exists [%s,%s,%s,%s,%s]...", name, country, state, city,
+                neighborhood));
+        Place[] places = placeStore.retrieve("", country, state, city, neighborhood, name);
 
-        if(places == null)
+        if(places == null) {
+            log.warn("No items were found!");
             return new PlaceResponse(null, "Error occurred retrieving places");
-        else
+        } else {
+            log.info(String.format("Found %d places", places.length));
             return new PlaceResponse(places, "success");
+        }
     }
 
     /**
-     * Add a new place to the system
+     * Get place REST request
+     *
+     * @return response
+     */
+    @RequestMapping(value = "/place/{id}", method = RequestMethod.GET)
+    public PlaceResponse getPlace(@PathVariable("id") String id){
+        log.info(String.format("Checking if Place already exists [%s]...", id));
+        Place[] places = placeStore.retrieve(id, "", "", "", "", "");
+
+        if(places == null) {
+            log.warn("No items were found!");
+            return new PlaceResponse(null, "Error occurred retrieving places");
+        } else {
+            log.info(String.format("Found %d places", places.length));
+            return new PlaceResponse(places, "success");
+        }
+    }
+
+    /**
+     * Update place in the system
+     *
      * @param place place information
      * @return response
      */
-    @RequestMapping(value = "/place", method = RequestMethod.PUT)
-    public PlaceResponse setPlace(@RequestBody Place place){
-        Place[] places = placeStore.retrieve("", place.getCountry(), place.getState(), place.getCity(),
-                place.getNeighborhood(), place.getName());
+    @RequestMapping(value = "/place/{id}", method = RequestMethod.POST)
+    public PlaceResponse setPlace(@PathVariable("id") String id,
+                                  @RequestBody Place place){
 
-        if(places != null) {
-            System.out.println(String.format("Already exists: %s", place.getName()));
-            return new PlaceResponse(null, "already exists");
-        }
+        log.info(String.format("Checking if Place exists [%s,%s,%s,%s,%s,%s]...", id, place.getName(),
+                place.getCountry(), place.getState(), place.getCity(), place.getNeighborhood()));
 
-        if(placeStore.insert(place)) {
-            System.out.println(String.format("Added!: %s", place.getName()));
-            return new PlaceResponse(new Place[]{place}, "success");
-        }else {
-            System.out.println(String.format("Failed to Add: %s", place.getName()));
-            return new PlaceResponse(null, "failed");
-        }
-    }
-
-    /**
-     * Update place request call
-     *
-     * @param place place information
-     * @return Response
-     */
-    @RequestMapping(value = "/place", method = RequestMethod.POST)
-    public PlaceResponse updatePlace(@RequestBody Place place){
         Place[] places = placeStore.retrieve(place.getId(), place.getCountry(), place.getState(), place.getCity(),
                 place.getNeighborhood(), place.getName());
 
-        if(places == null) {
-            System.out.println(String.format("Already exists: %s", place.getName()));
-            return new PlaceResponse(null, "unknown place");
+        if(places != null) {
+            log.info(String.format("Updating {%s}", place.getName()));
+
+            if(placeStore.update(place)) {
+                log.info(String.format("Updated!: %s", place.getId()));
+                return new PlaceResponse(new Place[]{place}, "success");
+            }else {
+                log.info(String.format("Failed to update: %s", place.getName()));
+                return new PlaceResponse(null, "failed");
+            }
         }
 
-        if(placeStore.update(place))
-            return new PlaceResponse(new Place[]{place}, "success");
-        else
-            return new PlaceResponse(null, "failed");
-    }
-
-    /**
-     * Retrieve features
-     *
-     * @param idStr id string
-     * @param name feature name
-     * @param category feature category
-     * @return features
-     */
-    @RequestMapping(value = "/place/feature", method = RequestMethod.GET)
-    public FeatureResponse getFeature(@RequestParam(value = "id", defaultValue = "")String idStr,
-                                       @RequestParam(value = "name", defaultValue = "")String name,
-                                       @RequestParam(value = "category", defaultValue = "")String category){
-        Feature[] features = featureStore.retrieve(idStr, name, category);
-
-        if(features == null)
-            return new FeatureResponse(null, "Error occurred retrieving places");
-        else
-            return new FeatureResponse(features, "success");
+        log.info(String.format("Failed to Update: %s", place.getName()));
+        return new PlaceResponse(null, "failed");
     }
 
     /**
@@ -125,21 +155,63 @@ public class PlaceController {
      * @param feature feature to add
      * @return status
      */
-    @RequestMapping(value = "/place/feature", method = RequestMethod.PUT)
+    @RequestMapping(value = "/place/feature", method = { RequestMethod.PUT, RequestMethod.POST })
     public FeatureResponse setFeature(@RequestBody Feature feature){
-        Feature[] features = featureStore.retrieve("", feature.getName(), feature.getCategory());
+        log.info(String.format("Checking if Feature exists [%s,%s]...", feature.getName(), feature.getCategory()));
+        Feature[] features = featureStore.retrieve(feature.getId(), feature.getName(), feature.getCategory());
 
         if(features != null) {
-            System.out.println(String.format("Already exists: %s", feature.getName()));
+            log.info(String.format("Feature already exists: {%s}", feature.getName()));
             return new FeatureResponse(null, "already exists");
         }
 
         if(featureStore.insert(feature)) {
-            System.out.println(String.format("Added!: %s", feature.getName()));
+            log.info(String.format("Feature was added: {%s}", feature.getId()));
             return new FeatureResponse(new Feature[]{feature}, "success");
         }else {
-            System.out.println(String.format("Failed to Add: %s", feature.getName()));
+            log.info(String.format("Failed to add feature: {%s}", feature.getName()));
             return new FeatureResponse(null, "failed");
+        }
+    }
+
+    /**
+     * Retrieve features
+     *
+     * @param name feature name
+     * @param category feature category
+     * @return features
+     */
+    @RequestMapping(value = "/place/feature", method = RequestMethod.GET)
+    public FeatureResponse getFeature(@RequestParam(value = "name", defaultValue = "")String name,
+                                      @RequestParam(value = "category", defaultValue = "")String category){
+        log.info(String.format("Checking if Feature exists [%s,%s]...", name, category));
+        Feature[] features = featureStore.retrieve("", name, category);
+
+        if(features == null) {
+            log.warn("no features were found!");
+            return new FeatureResponse(null, "Error occurred retrieving features");
+        } else {
+            log.info(String.format("Found %d features", features.length));
+            return new FeatureResponse(features, "success");
+        }
+    }
+
+    /**
+     * Retrieve feature
+     *
+     * @return feature
+     */
+    @RequestMapping(value = "/place/feature/{id}", method = RequestMethod.GET)
+    public FeatureResponse getFeature(@PathVariable("id") String id){
+        log.info(String.format("Retrieving Feature [%s]...", id));
+        Feature[] features = featureStore.retrieve(id, "", "");
+
+        if(features == null) {
+            log.warn("no features were found!");
+            return new FeatureResponse(null, "Error occurred retrieving feature");
+        } else {
+            log.info(String.format("Found %d features", features.length));
+            return new FeatureResponse(features, "success");
         }
     }
 }
